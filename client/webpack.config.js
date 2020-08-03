b7f73ce531903bef84051907064f858d8166d9ed
Ryoji Yoshida <ryoji.ysd@gmail.com>
2020-08-03 20:12:46 +0900

    Fix webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,10 +5,11 @@ module.exports = {
   entry: path.resolve(__dirname, "./src/index.tsx"),
   output: {
     path: path.resolve(__dirname, "public/js"),
+    publicPath: '/js/',
     filename: "bundle.js"
   },
   devtool: "source-map",
-  target: "node",
+  target: "web",
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"]
   },
@@ -29,6 +30,7 @@ module.exports = {
     contentBase: path.resolve(__dirname, 'public'),
     historyApiFallback: true,
     hot: true,
+    watchContentBase: true,
     port: 3000,
     open: false
   }
